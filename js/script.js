b4f30af90200c111a0c1caa537a2d81caa8ab3c4
Sachin <sachinsharma972097@gmail.com>
2023-06-21 00:40:07 +0530

    second commit

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+function myFunction(){
+    const showMenu = document.getElementById('menu');
+    if (showMenu.style.display === "none") {
+        showMenu.style.display = "block";
+      } else {
+        showMenu.style.display = "none";
+      }
+}
+
+
+function linkAction(){
+    const showMenu = document.getElementById('menu')
+    if (showMenu.style.display === "block") {
+        showMenu.style.display = "none";
+      }
+}
+
